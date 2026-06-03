0a622b4174f8ae4ae0e07bd1b52bcfb8445dd9c3
Giritzu <gh2801023@gmail.com>
2026-06-03 00:28:09 -0400

    add logic of the menu

--- MDDE.py
+++ MDDE.py
@@ -4,5 +4,15 @@ import os
 
 #pestaña principal
 pg.init()
-screen = pg.display.set_mode(720,1280)
+screen = pg.display.set_mode((1280,720))
 titulo = pg.display.set_caption("MDDE")
+time = pg.time.Clock()
+run = True
+
+#codigo principal
+while run == True:
+    #si le dan a quit para poder salir del juego
+    for evento in pg.event.get():
+        if evento.type == pg.QUIT:
+            run = False
+pg.quit()
