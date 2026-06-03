7e7653035c556a6e1151ce43a84eeac08e3605f9
Giritzu <gh2801023@gmail.com>
2026-06-03 04:04:40 -0400

    'buttons' aggred

--- archivos_necesarios/botones.py
+++ archivos_necesarios/botones.py
@@ -9,10 +9,35 @@ class buttons:
 
         #el texto
         self.text = pg.font.SysFont("Arial",20)
-        self.text_render = self.text.render("Play",True,constantes.NEGRO)
+        self.text_render = self.text.render(None,True,constantes.NEGRO)
         self.text_coords = screen.blit(self.text_render,(0,0))
 
 class boton_play(buttons):
     def __init__(self, screen):
         super().__init__(screen)
-        self.text_coords = screen.blit(self.text_render,(50,340))
+        self.text_render = self.text.render("Nuevo juego",True, constantes.NEGRO)
+        self.text_coords = screen.blit(self.text_render,(100,340))
+
+class boton_load(buttons):
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.text_render = self.text.render("Cargar datos",True, constantes.NEGRO)
+        self.text_coords = screen.blit(self.text_render,(150,340+70))
+
+class boton_extras(buttons):
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.text_render = self.text.render("Extras",True, constantes.NEGRO)
+        self.text_coords = screen.blit(self.text_render,(150+50,340+70+70))
+
+class boton_opciones(buttons):
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.text_render = self.text.render("Opciones",True, constantes.NEGRO)
+        self.text_coords = screen.blit(self.text_render,(150+50+50,340+70+70+70))
+
+class boton_exit(buttons):
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.text_render = self.text.render("Salir",True, constantes.NEGRO)
+        self.text_coords = screen.blit(self.text_render,(150+50+50+50,340+70+70+70+70))
