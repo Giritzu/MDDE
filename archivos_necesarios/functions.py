8014e365a273ade70129ed195bc36b8a80120dbc
Gritzu <gh2801023@gmail.com>
2026-06-08 01:13:46 -0400

    creations of functions

---
+++ archivos_necesarios/functions.py
@@ -0,0 +1,17 @@
+import pygame as pg
+import os
+from archivos_necesarios import main_menu
+from archivos_necesarios import botones
+from archivos_necesarios import constantes
+
+##carpeta meramente hecha para tener todas las funciones necesarias para reciclar codigo
+
+##funcion para los rectangulos (hitboxes) de los botones y todo lo que se vaya a usar relacionados con estos
+def position_rect(pantalla,color_contorno,x,y,ancho,alto):
+    
+    pass
+
+##funcion para los textos
+def text_renders(text,pantalla, text_color,text_option,x,y):
+    
+    pass
