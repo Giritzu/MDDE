a60f278ffabea07b7df6fe10111c77f9163c62b3
Gritzu <gh2801023@gmail.com>
2026-06-12 09:34:40 -0400

    add archive 'caja_de_texto'

---
+++ archivos_necesarios/caja_de_texto.py
@@ -0,0 +1,11 @@
+#importaciones
+import pygame as pg
+import os
+from archivos_necesarios import main_menu
+from archivos_necesarios import botones
+from archivos_necesarios import constantes
+from archivos_necesarios import functions
+
+class text_box:
+    def __init__(self):
+        pass
